cd90243d66fed88115ffe6d37ee8cca71a6f272d
carlosponton <carlos.ponton2@gmail.com>
2017-11-19 20:38:29 -0500

    Integración del menú con la seguridad

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -2,7 +2,8 @@ class InicioController < ApplicationController
 	include Servicios
 	def casa
 		idrol = session[:idrol]
-		@opciones = Opcione.all.solo_activos.order_opcions
+		@opciones = Opcione.joins("INNER JOIN rol_opcion_operaciones ON rol_opcion_operaciones.idoperacion = opciones.id").where("rol_opcion_operaciones.idrol = #{idrol} and rol_opcion_operaciones.estadorolop = 'A' and opciones.estadoopcion = 'A'")
+		pp @opciones
 		session[:opciones] =  @opciones
 		@html = "<ul id='tree1'>"
 		@opciones.each do |o|
